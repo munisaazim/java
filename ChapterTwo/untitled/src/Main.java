081cc890f9d6e121b3a5a570412fbbd8c70db827
munisaazim <munisa2000azim@gmail.com>
2022-10-21 00:38:36 +0500

    Adding new tasks to Chapter2

--- ChapterTwo/untitled/src/Main.java
+++ ChapterTwo/untitled/src/Main.java
@@ -107,7 +107,7 @@ public class Main {
             int totalDay = (int)(day%365);
             System.out.println(minutes + " = " + totalYear + " years " + totalDay + " days");
 
-        }
+    }
         public void Task9(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter v0, v1, t");
@@ -116,7 +116,7 @@ public class Main {
         double t = input.nextDouble();
         double acc = (v1 - v0) /t;
         System.out.println("The average acceleration: " + acc);
-        }
+    }
         public void Task10(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the amount of water in kilograms:");
@@ -127,7 +127,7 @@ public class Main {
         double finalTemperature = input.nextDouble();
         double result = weight * (finalTemperature - initTemperature) * 4184;
         System.out.println("The energy needed is " + result);
-        }
+    }
         public void Task11(){
             double yearSecond = 365 * 86400;
             double birth = yearSecond / 7.0;
@@ -136,7 +136,7 @@ public class Main {
             double allCalculationOneYear = ((yearSecond * 7)  - death) + migrate;
             int allCalculationFiveYear = (int)(allCalculationOneYear * 5);
             System.out.println("Calculation of possible population for following 5 years: " + allCalculationFiveYear);
-        }
+    }
         public void Task12(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter speed and acceleration: ");
@@ -144,13 +144,67 @@ public class Main {
         double acceleration = input.nextDouble();
         double length = (speed * speed)/(2 * acceleration);
         System.out.println("The minimum runway length for this airplane is " + length);
-        }
+    }
         public void Task13(){
-        Scanner in
-        }
+            Scanner input = new Scanner(System.in);
+            System.out.println("Add your savings: ");
+            double savings = input.nextDouble();
+            double annualInterest = 5 /savings;
+            //System.out.println("annual" + annualInterest);
+            double monthInterestRate = annualInterest /12;
+            double firstMonth = 100 * (1 + monthInterestRate);
+            //System.out.println("firstmonth" + firstMonth);
+            double secondMonth = (100 + firstMonth) * (1 + monthInterestRate);
+            double thirdMonth = (100 + secondMonth) * (1 + monthInterestRate);
+            double fourthMonth = (100 + thirdMonth) * (1 + monthInterestRate);
+            double fifthMonth = (100 + fourthMonth) * (1 + monthInterestRate);
+            double sixthMonth = (100 + fifthMonth) * (1 + monthInterestRate);
+            //double seventhMonth = (100 + sixthMonth) * (1 + monthInterestRate);
+            System.out.println("After the sixth month, the account value is $" + sixthMonth);
+    }
+        public void Task14(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter weight(pounds)");
+        double pound = input.nextDouble();
+        System.out.println("Enter height(inches)");
+        double inch = input.nextDouble();
+        double kilogramm = pound * 0.45359237;
+        double meters = inch * 0.0254;
+        double BMI = kilogramm / (meters * meters);
+        System.out.println("BMI: " + BMI);
+    }
+    public void Task15(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter x1 and y1");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        System.out.println("Neter x2 and y2");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        double x = (x2-x1) * (x2 - x1);
+        double y = (y2 - y1) * (y2 - y1);
+        double sum = x + y;
+        double distance = Math.pow(sum, 0.5);
+        System.out.println("The distance between the two points is " + distance);
+    }
+    public void Task16(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter side");
+        double side = input.nextDouble();
+        double area = (3 * Math.pow(3, 0.5)/2) * Math.pow(side, 2);
+        System.out.println("The area of the hexagon is " + area);
+    }
+    public void Task17(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the temperature in Fahrenheit between -58°F and 41°F:");
+        double temperature = input.nextDouble();
+        System.out.println("Enter the wind speed (>=2) in miles per hour");
+        double speed = input.nextDouble();
+        double solution = ((35.74 + (0.625 * temperature)) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * temperature * (Math.pow(speed, 0.16))));
+        System.out.println("The wind chill index is " + solution);
+    }
     public static void main(String[] args) {
         Main object = new Main();
-        object.Task12();
-
+        object.Task17();
     }
 }
