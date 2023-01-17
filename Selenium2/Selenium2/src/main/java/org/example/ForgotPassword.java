7c04eb12235db1be667a565f58016e01ac835c22
munisaazim <munisa2000azim@gmail.com>
2023-01-17 11:49:16 +0500

    Adding new functionality

--- Selenium2/Selenium2/src/main/java/org/example/ForgotPassword.java
+++ Selenium2/Selenium2/src/main/java/org/example/ForgotPassword.java
@@ -10,52 +10,34 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class ForgotPassword {
     public WebDriver driver = new ChromeDriver();
-    int waitNumber = 15;
-
-    Duration seconds = Duration.ofSeconds(15);
-    //WebDriverWait wait=new WebDriverWait(driver1, seconds);
-
-
     public void forgotPassword(){
-
-       /* Login loginObject = new Login();
-
-        loginObject.enterPage();
-
-        loginObject.onBoardingWelcomeWithoutMain();
-
-        loginObject.clickOnButtonSignIn();
-
-        //wait.until(ExpectedConditions.textToBePresentInElement((WebElement) By.xpath("//*[@class = 'forgot']"), " Forgot your password? "));
-        driver1.findElement(By.xpath("//*[text()= \" Forgot your password? \"]")).click();
-        //loginObject.driver.findElement(By.xpath("//*[@type = 'email']")).sendKeys("munisa2000azim@gmail.com");
-        driver1.findElement(By.xpath("//*[ text() = 'Back to sign in']")).click();
-
-        loginObject.loginWithoutOnBoarding();
-
-        System.out.println("Passed check of Forgot page");
-
-
-        */
-
-
         driver.get("https://staging.engineer.ai/home");
 
         driver.findElement(By.xpath("//button[text() = \"Choose a base\"]")).click();
-        driver.findElement(By.xpath("//*[@class = \"closeButton\"]")).click();
+
+        driver.findElement(By.xpath("(//*[@class = 'icon-cancel'])[4]")).click();
         driver.findElement(By.xpath("//*[@class = \"natashPanelOverlay\"]")).click();
+        driver.findElement(By.xpath("//*[@type = 'button'][1]")).click();
+        if(driver.findElement(By.xpath("//*[@class = 'forgot']")).isDisplayed());
+        {
+            driver.findElement(By.xpath("//*[@class = 'forgot']")).click();
+            driver.findElement(By.xpath("//*[@class = 'forgot']")).click();
+            driver.findElement(By.xpath("//*[@type = 'email']")).sendKeys("munisa2000azim@gmail.com");
+            if(driver.findElement(By.xpath("//*[@class = 'alreadyHave login']")).isDisplayed());{
+            driver.findElement(By.xpath("//*[@class = 'linkcolor']")).click();
+            if(driver.findElement(By.xpath("//*[@class = 'new-CloseButton']")).isDisplayed());{
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                driver.findElement(By.xpath("(//*[@class = 'icon-cancel'])[4]")).click();
+                System.out.println("Hello");
+            }
 
-        //driver.findElement(By.xpath("//*[@type = 'button'][1]")).click();
+        }
 
-        //wait.until(ExpectedConditions.textToBePresentInElement((WebElement) By.xpath("//*[@class = 'forgot']"), " Forgot your password? "));
-        driver.findElement(By.xpath("//*[text()= \" Forgot your password? \"]")).click();
-        //loginObject.driver.findElement(By.xpath("//*[@type = 'email']")).sendKeys("munisa2000azim@gmail.com");
-        driver.findElement(By.xpath("//*[ text() = 'Back to sign in']")).click();
-
-
+        }
 
         System.out.println("Passed check of Forgot page");
 
